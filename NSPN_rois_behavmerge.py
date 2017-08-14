339f4d1a2c46bb5b3a52f2483dd74476dc38a05c
Kirstie Whitaker <kw401@cam.ac.uk>
2017-08-14 12:02:32 +0100

    Line up printouts for AssignLobes w other commands
    I've been fiddling around with the printout for all the different
    extract stats & expand surfaces printout so that it's easy to follow
    along in a log file or as the code is running to see where we're at.
    
    It was annoying me that these print outs were indented further than the
    new ones, so I've pushed them back :)

--- NSPN_rois_behavmerge.py
+++ NSPN_rois_behavmerge.py
@@ -15,6 +15,9 @@ which is created in the same folder as the behavioural file
 #=============================================================================
 # IMPORTS
 #=============================================================================
+# Make this code python 2 and 3 compatible
+from __future__ import print_function
+
 import pandas as pd
 import numpy as np
 from glob import glob
@@ -28,7 +31,7 @@ import sys
 def usage():
     print "USAGE freesurfer_rois_behavmerge.py <FS_ROIS_DIR> <BEHAV_FILE>"
     sys.exit()
-    
+
 #=============================================================================
 # READ IN COMMAND LINE ARGUMENTS
 #=============================================================================
@@ -36,7 +39,7 @@ def usage():
 if len(sys.argv) < 2:
     print "Not enough arguments passed"
     usage()
-    
+
 # Get the fs_rois_dir from the command line
 if not sys.argv[1]:
     print "Can't find FS_ROIS directory"
@@ -66,20 +69,20 @@ print suffix
 # MERGE MEASURES WITH BEHAV VALUES
 #=============================================================================
 # Create a list of the freesurfer measures
-measure_list = [ 'mean', 'area', 
+measure_list = [ 'mean', 'area',
                     'volume', 'thickness',
                     'thicknessstd',
-                    'meancurv', 'gauscurv', 
+                    'meancurv', 'gauscurv',
                     'foldind', 'curvind',
                      'std' ]
 
 # Create an empty file list
 file_list = []
 
-# Loop through all the measures, find all the files that end with 
+# Loop through all the measures, find all the files that end with
 # those words and add them to the file list
 for measure in measure_list:
-    
+
     file_list += glob(os.path.join(fs_rois_dir, '*{}.csv'.format(measure)))
 
 # Loop through the files
@@ -88,36 +91,35 @@ for f in file_list:
     # Check the number of lines that are in the file
     with open(f) as fid:
         num_lines = len(fid.readlines())
-        
+
     # And only try to merge files that have content
     if num_lines > 0:
-        
+
         # Read the csv roi file into a data frame
         df_meas = pd.read_csv(f)
-        
+
         if 'nspn_id' in df_meas.columns:
-            
+
             # Merge on 'nspn_id' and 'occ'
             df = df_behav.merge(df_meas, on=['nspn_id', 'occ'])
-            
+
             # Sort into ascending nspn_id
             df.sort('nspn_id', inplace=True)
 
-            # Drop columns containing the word 'Measure' 
+            # Drop columns containing the word 'Measure'
             # if they exist
             c_drop = [ x for x in df.columns if 'Measure' in x ]
             if c_drop:
                 df.drop(c_drop, inplace=True, axis=1)
-            
+
             # Create an output file name that removes any '.' symbols
             # in the file name
             f_name = os.path.basename(f)
             f_out = f_name.replace('.', '')
             # and appends '_behavmerge.csv'
             f_out = f_out.replace('csv', '_{}.csv'.format(suffix))
-            
+
             # Put this file in the same folder as the behavioural file
             behav_dir = os.path.dirname(behav_file)
             f_out = os.path.join(behav_dir, f_out)
             df.to_csv(f_out,float_format='%.5f')
-            
